69d97588e2e8aaa7ac632ffd8ddc3f975c0b19a3
Deva543 <kumardeva299@gmail.com>
2022-09-09 10:13:16 +0530

    Added Add method to Calculator class

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,9 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Calculator");
     }
+
+    public void Add() {
+        System.out.println("Inside Add method");
+
+    }
 }
